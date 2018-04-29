787d828401997e20a434daea89752c6f8d481b87
Chris Sreesangkom <csreesan@berkeley.edu>
2018-04-29 14:46:25 -0700

    solver

--- solver_for_hive5.py
+++ solver_for_hive5.py
@@ -2,4 +2,5 @@ from solver_specific import solver
 
 file_list = ['323', '325', '345', '348', '350', '370', '371', '379', '381', '382', '383', '387', '388', '390', '395', '398', '405', '411', '412', '413', '420']
 
+file_list = [675]
 solver("./curr_hive5.txt", "./iter_hive5.txt", "./beaten_hive5.txt", "./outputs/", False, file_list)
